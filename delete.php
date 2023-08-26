4f6d49e92e353c8d466f6f4e85837b743d55469f
Niushaaaa <117599554+Niushaaaa@users.noreply.github.com>
2023-08-27 00:17:24 +0330

    dorost krdn delete ba switch va query krdm

---
+++ delete.php
@@ -0,0 +1,20 @@
+<?php session_start(); ob_start();
+include "connection.php";
+if (isset($_GET['id']) && isset($_GET['page'])) {
+    $id = $_GET['id'];
+    $page = $_GET['page'];
+    switch ($page) {
+        case 1:
+            $query = "DELETE FROM class WHERE class_id=" . $id;
+            $del = $db->prepare($query);
+            $del->execute();
+            header("location:index.php");
+            break;
+        case 2:
+            $query = "DELETE FROM student WHERE stud_id=" . $id;
+            $del = $db->prepare($query);
+            $del->execute();
+            header("location:index.php");
+            break;
+    }
+}
